d25df058425a45d718e3cfa3ab9e2f5738b7602c
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-03-16 16:17:06 +0900

    Add 2525, 2480, 18108, 10757 BOJ

---
+++ BOJ/Implementation/10757.py
@@ -0,0 +1,7 @@
+# BOJ 10757번 문제: 큰 수 A+B
+def solution():
+    answer = sum(list(map(int, input().split())))
+    print(answer)
+
+if '__main__' == __name__:
+    solution()
---
+++ BOJ/Implementation/18108.py
@@ -0,0 +1,9 @@
+# BOJ 18108번 문제: 1998생인 내가 태국에서는 2541년생?!
+def solution():
+    n = int(input())
+
+    print(n - 543)
+
+if '__main__' == __name__:
+    solution()
+
