949e0760aefb33879332e010efb2e349ea859aaf
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-04-04 14:48:34 +0900

    BOJ 10866

---
+++ BOJ/Queue_Deque/10866.py
@@ -0,0 +1,43 @@
+# BOJ 10866번 문제: 덱
+from collections import deque
+from sys import stdin
+def solution():
+    dequeue = deque()
+
+    N = int(stdin.readline()) # 1 <= N <= 10000
+    for _ in range(N):
+        command = stdin.readline().split()
+        if command[0] == 'push_back':
+            dequeue.append(int(command[1]))
+        elif command[0] == 'push_front':
+            dequeue.appendleft(int(command[1]))
+        elif command[0] == 'pop_front':
+            if len(dequeue):
+                print(dequeue.popleft())
+            else:
+                print(-1)
+        elif command[0] == 'pop_back':
+            if len(dequeue):
+                print(dequeue.pop())
+            else:
+                print(-1)
+        elif command[0] == 'size':
+            print(len(dequeue))
+        elif command[0] == 'empty':
+            print(int(not len(dequeue)))
+        elif command[0] == 'front':
+            if len(dequeue):
+                print(dequeue[0])
+            else:
+                print(-1)
+        elif command[0] == 'back':
+            if len(dequeue):
+                print(dequeue[-1])
+            else:
+                print(-1)
+
+
+
+
+if '__main__' == __name__:
+    solution()
